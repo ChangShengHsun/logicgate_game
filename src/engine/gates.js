a76d04b241e5f476a943a49bde24ea93d9a4bfa2
ivan chang <ic046262@gmail.com>
2026-01-12 22:05:02 +0800

    add gates.js

---
+++ src/engine/gates.js
@@ -0,0 +1,12 @@
+export const GATES = {
+  buffer: (a) => a,
+  not:    (a) => a ^ 1,
+
+  and:  (a,b) => a & b,
+  or:   (a,b) => a | b,
+  xor:  (a,b) => a ^ b,
+
+  nand: (a,b) => (a & b) ^ 1,
+  nor:  (a,b) => (a | b) ^ 1,
+  xnor: (a,b) => (a ^ b) ^ 1,
+};
